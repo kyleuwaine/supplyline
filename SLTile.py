2142466048ee5b3030ce129bc266f32680123c00
kyle uwaine <kyleuwaine@gmail.com>
2022-07-24 19:01:36 -0700

    Added type class variable

--- SLTile.py
+++ SLTile.py
@@ -13,11 +13,10 @@ class SLTile:
 
     def __init__(self, top_left_corner, full_screen_mask, tile_type):
         if (tile_type == SLTile.Type.STANDARD): 
-            self.type = tile_type
             self.sprite = "Images\grass_05.png"
         elif (tile_type == SLTile.Type.BORDER):
-            self.type = tile_type
             self.sprite = "Images\dirt_06.png"
+        self.type = tile_type
         self.occupant = None
         self.top_left_corner = top_left_corner
         self.pygame_surface = pygame.image.load(self.sprite)
