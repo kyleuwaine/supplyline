bd471a368cd42f05b00859989d8d3b6bc15986c5
Victor Nault <vnault@haverford.edu>
2022-07-31 20:30:11 -0700

    Created

---
+++ SLAI.py
@@ -0,0 +1,8 @@
+class SLAI:
+    def __init__(self, faction: SLFaction):
+        self.faction = faction
+
+    def AI_turn():
+        for brigade in self.faction.brigade_list:
+            #move_brigade_randomly()
+            brigade.location.location
