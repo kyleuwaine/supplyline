efca72787a8481cf12e924828d0aeb98c61da0a1
Victor Nault <vnault@haverford.edu>
2022-07-31 19:36:41 -0700

    Moved create_init_brigades() code from supply_line.py, added create faction functionality

--- startup.py
+++ startup.py
@@ -1,5 +1,20 @@
 import pygame
+from SLFaction import SLFaction
+from SLBrigade import SLBrigade
 from SLTile import SLTile
+
+def create_init_brigades(faction_list: list, tile_grid: list, screen):
+    tile_grid[1][1].occupant = SLBrigade("Tank", faction_list[0])
+    screen.blit(tile_grid[1][1].occupant.pygame_surface, tile_grid[1][1].top_left_corner)
+    tile_grid[2][2].occupant = SLBrigade("Tank", faction_list[1])
+    screen.blit(tile_grid[2][2].occupant.pygame_surface, tile_grid[2][2].top_left_corner)
+
+def create_factions(num_of_factions: int):
+    faction_list = []
+    for i in range(num_of_factions):
+        faction_list.append(SLFaction(None, i))
+    return faction_list
+
 
 def find_topleft(screen_width, screen_height, hex_sprite_width, hex_sprite_height, grid_width, grid_height):
     assert grid_width % 2 != 0, "expected odd grid width"
@@ -22,7 +37,7 @@ def prepare_map(screen, screen_width, screen_height, full_screen_mask_input):
     tile_grid = [[0 for x in range(tile_grid_width)] for y in range(tile_grid_height)]
     #tile_grid[0][0] = SLTile((0, 0))
     #tile_grid[0][0] = SLTile(( (screen_width // 2) - (hex_sprite_width // 2) , (screen_height // 2) - (hex_sprite_height // 2) ), full_screen_mask_input)
-    is_offset = True # Need offset on every other row to properly create a hex map 
+    is_offset = True # Need offset on every other row to properly create a hex map
     is_skip_last_hex = True
     offset = hex_sprite_width // 2
     for i in range(tile_grid_width): # Loop to fill out tile_grid and render all tiles onto screen
@@ -35,7 +50,7 @@ def prepare_map(screen, screen_width, screen_height, full_screen_mask_input):
                     break
                 else:
                     is_skip_last_hex = True
-            if (is_offset): 
+            if (is_offset):
                 if ((j == 0) or (j == tile_grid_width - 2)):
                     tile_grid[i][j] = SLTile((current_x + offset, y), full_screen_mask_input.copy(), SLTile.Type.BORDER)
                 elif ((i == 0) or (i == tile_grid_height - 1)):
@@ -73,6 +88,10 @@ def startup():
     full_screen_surface = pygame.Surface((screen_width, screen_height))
     full_screen_mask = pygame.mask.from_surface(full_screen_surface)
     full_screen_mask.invert()
+    num_of_factions = 2
+    faction_turn = 0
+    faction_list = create_factions(num_of_factions)
     tile_grid, tile_grid_size = prepare_map(screen, screen_width, screen_height, full_screen_mask)
+    create_init_brigades(faction_list, tile_grid, screen)
 
-    return clock, framerate, screen, tile_grid, tile_grid_size
+    return clock, framerate, screen, tile_grid, tile_grid_size, faction_turn, num_of_factions
