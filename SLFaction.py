82508f1f085a0ca608ae65d6385552d993aa1777
Victor Nault <vnault@haverford.edu>
2022-07-31 20:30:35 -0700

    Added brigade_list instance variable

--- SLFaction.py
+++ SLFaction.py
@@ -1,4 +1,8 @@
+import pygame
+
 class SLFaction:
-    def __init__(self, name: str, new_id: int):
-        self.name = name
+    def __init__(self, new_name: str, new_id: int, new_color: pygame.Color, new_brigade_list: list):
+        self.name = new_name
         self.id = new_id
+        self.color = new_color
+        self.brigade_list = new_brigade_list
