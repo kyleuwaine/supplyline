8174d52e36d9eb60ba88eb4a6d42432ddb37bf7e
Victor Nault <vnault@haverford.edu>
2022-08-07 19:09:20 -0700

    Added comments

--- SLAI.py
+++ SLAI.py
@@ -3,12 +3,18 @@ import SLFaction
 from random import randrange
 
 class SLAI:
+    # Represents an AI opponent
+    # Parameters:   faction - the faction that the AI controls
+    #               map - the hex grid the game is being played on
+    #               screen - the pygame screen
+
     def __init__(self, faction: SLFaction, map, screen):
         self.faction = faction
         self.map = map
         self.screen = screen
 
     def AI_turn(self):
+        # Performs the AI turn by moving all of its brigades to random empty neighboring tiles
         for brigade in self.faction.brigade_list:
             possible_dests = game_functions.find_empty_neighbors(brigade.location, self.map)
             game_functions.move_occupant(brigade.location, possible_dests[randrange(0, len(possible_dests))], self.screen)
