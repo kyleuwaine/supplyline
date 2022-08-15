b9c1482aaede6e2ae120ae5192af1472b90032a3
kyle uwaine <kyleuwaine@gmail.com>
2022-08-14 19:44:46 -0700

    Changed brigade list to dict

--- SLFaction.py
+++ SLFaction.py
@@ -1,10 +1,10 @@
 import pygame
 
 class SLFaction:
-    def __init__(self, new_name: str, new_id: int, new_color: pygame.Color, new_brigade_list: list):
+    def __init__(self, new_name: str, new_id: int, new_color: pygame.Color, new_brigade_dict: list):
         self.name = new_name
         self.id = new_id
         self.color = new_color
-        self.brigade_list = new_brigade_list
+        self.brigade_dict = new_brigade_dict
         self.brigade_counter = 0
         self.building_counter = 0
